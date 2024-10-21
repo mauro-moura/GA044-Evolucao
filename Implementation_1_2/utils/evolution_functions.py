eb8a24594366f9d67f8856d16938614c4ee1ac3c
Mauro Sérgio <maurosergiostm@gmail.com>
2024-10-21 11:52:48 -0300

    Finished implementation 1

--- Implementation_1_2/utils/evolution_functions.py
+++ Implementation_1_2/utils/evolution_functions.py
@@ -27,7 +27,6 @@ def plot_n_pt(t, p_t_list, p0_list, u, v):
     plt.show()
 
 def plot_n_pt_qt(t, p_t_list, q_t_list, p0_list, q0_list, u, v):
-    counter = 0
     for p_t, q_t, p0, q0 in zip(p_t_list, q_t_list, p0_list, q0_list):
         plt.plot(t, p_t, label=f'p = {p0:.4f}')
         plt.plot(t, q_t, label=f'q = {q0:.4f}')
@@ -55,4 +54,34 @@ def plot_pt_qt(t, p_t, q_t, p0, q0):
     plt.plot(t, q_t, label=f'q = {q0:.4f}')
     plt.legend()
     # plt.title(f'u = {u}, v = {v}')
+    plt.show()
+
+def plot_all(t, p_t_list, q_t_list, p2_t_list, q2_t_list, pq_t_list, chi_squared_list, p0_list, q0_list, u, v):
+    plt.figure(figsize=(16, 9))
+
+    plt.subplot(3, 3, 1)
+    counter = 0
+    for p_t, p0, q_t, q0 in zip(p_t_list, p0_list, q_t_list, q0_list):
+        counter += 1
+        plt.plot(t, p_t, label=f'p_{counter} = {p0:.4f}')
+        plt.plot(t, q_t, label=f'q_{counter} = {q0:.4f}')
+    plt.legend()
+
+    plt.subplot(3, 3, 2)
+    counter = 0
+    for p2_t, q2_t, pq_t in zip(p2_t_list, q2_t_list, pq_t_list):
+        counter += 1
+        plt.plot(t, p2_t, label=f'p²_{counter}')
+        plt.plot(t, q2_t, label=f'q²_{counter}')
+        plt.plot(t, pq_t, label=f'pq_{counter}')
+    plt.legend()
+
+    plt.subplot(3, 3, 3)
+    counter = 0
+    for chi_squared in chi_squared_list:
+        counter += 1
+        plt.plot(t, chi_squared, label=f'chi²_{counter}')
+    plt.legend()
+
+    plt.suptitle(f'u = {u}, v = {v}')
     plt.show()
